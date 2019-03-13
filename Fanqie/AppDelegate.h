8b398f9f269adff53da33b182bf252a52f0112a6
周建波 <trh_imac01@zhoujianbodeimac.local>
2019-03-13 10:13:40 +0800

    Initial Commit

---
+++ Fanqie/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Fanqie
+//
+//  Created by 周建波 on 2019/3/13.
+//  Copyright © 2019 周建波. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Fanqie/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Fanqie
+//
+//  Created by 周建波 on 2019/3/13.
+//  Copyright © 2019 周建波. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
